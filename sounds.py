337f885e7d80c33f0e3337e9732d4d8ea15f124a
Elia Argentieri <elia.argentieri@openmailbox.org>
2016-03-28 00:38:46 +0200

    Add config file to set sounds' volume

--- sounds.py
+++ sounds.py
@@ -6,15 +6,37 @@ import random
 
 
 __logger = logging.getLogger('Sounds')
+extensions = ['.ogg', '.wav']
 sounds = {}
+
+
+def parse_cfg(fpath):
+	print(fpath)
+	try:
+		with open(fpath, 'r') as f:
+			__logger.debug('Found config file %s' % fpath)
+			for line in f:
+				k, v = line.split('=')
+				if k == 'volume' and 0 <= float(v) <= 1.0:
+					sounds[root].set_volume(float(v))
+	except FileNotFoundError:
+		pass
+
 
 for f in resources.list_sounds():
 	p = os.path.join(resources.SOUNDS_PATH, f)
-	if os.path.isfile(p) and (f.endswith('.ogg') or f.endswith('.wav')):
-		sounds[f[:-4]] = pygame.mixer.Sound(p)
+	root, ext = os.path.splitext(f)
+	if os.path.isfile(p) and ext in extensions:
+		sounds[root] = pygame.mixer.Sound(p)
+		parse_cfg(os.path.join(resources.SOUNDS_PATH, root + '.cfg'))
 	elif os.path.isdir(p):
 		lsdir = os.listdir(p)
-		sounds[f] = [pygame.mixer.Sound(os.path.join(p, f)) for f in lsdir if f.endswith('.ogg') or f.endswith('.wav')]
+		sounds[f] = []
+		for fd in lsdir:
+			root, ext = os.path.splitext(fd)
+			if ext in extensions:
+				sounds[f].append(pygame.mixer.Sound(os.path.join(p, fd)))
+				parse_cfg(os.path.join(p, root + '.cfg'))
 
 __logger.debug("Sounds initialized!")
 
