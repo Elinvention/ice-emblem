8b329f0ef2d389ebc87bdeb9150181da3500b5f1
Elia Argentieri <elia@elinvention.ovh>
2018-10-02 22:11:48 +0200

    Add LiberationMono font family

--- fonts.py
+++ fonts.py
@@ -3,3 +3,5 @@ import resources
 MAIN_MENU = resources.load_font('Medieval Sharp/MedievalSharp.ttf', 48)
 MAIN = resources.load_font('Medieval Sharp/MedievalSharp.ttf', 36)
 SMALL = resources.load_font('Medieval Sharp/MedievalSharp.ttf', 24)
+MONOSPACE = resources.load_font("LiberationMono/LiberationMono-Regular.ttf", 18)
+
