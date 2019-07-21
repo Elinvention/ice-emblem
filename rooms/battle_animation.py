956214a85f07061a9025d11522c7def5cf7a2ce4
Elia Argentieri <elia@elinvention.ovh>
2019-07-21 20:35:51 +0200

    Improve battle_animation and add ability to skip animations.

--- rooms/battle_animation.py
+++ rooms/battle_animation.py
@@ -10,23 +10,25 @@ import state as s
 import sounds
 import colors as c
 
+from unit import Unit, Team
+from gui import Gravity
+
 
 class AttackAnimation(gui.Tween):
     def __init__(self, image, vector, on_animation_finished):
-        super().__init__(vector, 200, callback=on_animation_finished, die_when_done=False,
-                         bg_color=(0, 0, 0, 0))
-        self.text = gui.Label("", f.SMALL, visible=False, bg_color=(0, 0, 0, 0))
+        super().__init__(vector, 200, callback=on_animation_finished, die_when_done=False)
+        self.text = gui.Label("", f.SMALL, visible=False)
         self.add_children(self.text, image)
 
 
 class BattleUnitStats(gui.LinearLayout):
-    def __init__(self, unit, vector, on_animation_finished, **kwargs):
+    def __init__(self, unit: Unit, vector, on_animation_finished, **kwargs):
         super().__init__(padding=100, **kwargs)
         self.unit = unit
-        self.animation = AttackAnimation(gui.Image(unit.image), vector, on_animation_finished)
-        self.name = gui.Label(unit.name, f.MAIN, txt_color=unit.team.color, bg_color=(0, 0, 0, 0))
-        self.life = gui.LifeBar(max=unit.health_max, value=unit.health)
-        self.stats = gui.Label(str(unit), f.SMALL, bg_color=(0, 0, 0, 0))
+        self.animation = AttackAnimation(gui.Image(unit.image, die_when_done=False, bg_color=c.MENU_BG), vector, on_animation_finished)
+        self.name = gui.Label(unit.name, f.MAIN, txt_color=unit.team.color)
+        self.life = gui.LifeBar(points=unit.health_max, value=unit.health)
+        self.stats = gui.Label(str(unit), f.SMALL)
         self.add_children(self.animation, self.name, self.life, self.stats)
 
     def update(self):
@@ -34,15 +36,21 @@ class BattleUnitStats(gui.LinearLayout):
         self.stats.set_text(str(self.unit))
 
 
-class BattleAnimation(room.Room):
-    def __init__(self, attacking, defending, **kwargs):
-        super().__init__(wait=False, size=display.get_size(), **kwargs)
+def broken_screen(unit):
+    sounds.play('broke')
+    room.run_room(gui.Label("%s is broken" % unit.weapon.name, f.SMALL, txt_color=c.RED))
+
+
+class BattleAnimation(gui.LinearLayout):
+    def __init__(self, attacking: Unit, defending: Unit, **kwargs):
+        super().__init__(wait=False, orientation=gui.Orientation.HORIZONTAL, **kwargs)
         self.attacking = attacking
         self.defending = defending
-        drect = display.get_rect()
-        self.att_stats = self.att_swap = BattleUnitStats(self.attacking, (50, 0), self.anim_finished, center=(drect.centerx-drect.centerx//2, drect.centery), bg_color=c.BLACK)
-        self.def_stats = self.def_swap = BattleUnitStats(self.defending, (-50, 0), self.anim_finished, center=(drect.centerx+drect.centerx//2, drect.centery), bg_color=c.BLACK)
+        self.att_stats = self.att_swap = BattleUnitStats(self.attacking, (50, 0), self.anim_finished, layout_gravity=Gravity.CENTER)
+        self.def_stats = self.def_swap = BattleUnitStats(self.defending, (-50, 0), self.anim_finished, layout_gravity=Gravity.CENTER)
         self.add_children(self.att_stats, self.def_stats)
+        self.at = self.dt = self.round = 0
+        self.outcome = self.damage = None
 
     def begin(self):
         super().begin()
@@ -58,12 +66,14 @@ class BattleAnimation(room.Room):
         print(att_str % (self.defending.name, self.dt, _("time") if self.dt == 1 else _("times")))
         self.attacking.team.play_music('battle')
 
-        room.run_room(rooms.Fadeout(1000, stop_mixer=False))
+        room.run_room(rooms.Fadeout(2000, stop_mixer=False))
 
     def anim_finished(self, tween):
-        left = tween.rect.left
         tween.go_backward(False)
         tween.callback = None
+        self.show_outcome()
+
+    def show_outcome(self):
         sounds.play(self.outcome)
         if self.outcome == 'hit':
             text = str(self.damage)
@@ -71,14 +81,35 @@ class BattleAnimation(room.Room):
             text = '%s %d' % (_(self.outcome.upper()), self.damage)
         else:
             text = _(self.outcome.upper())
-        tween.text.txt_color = c.RED
+        self.att_swap.animation.text.txt_color = c.RED
         if self.outcome == 'miss':
-            tween.text.txt_color = c.YELLOW
-        tween.text.set_text(text)
-        tween.text.visible = True
+            self.att_swap.animation.text.txt_color = c.YELLOW
+        self.att_swap.animation.text.set_text(text)
+        self.att_swap.animation.text.visible = True
         self.att_swap.update()
         self.def_swap.update()
-        tween.rect.left = left  # Container may reflow this child
+
+    def handle_mousebuttondown(self, _event: pygame.event.EventType):
+        if _event.button == pygame.BUTTON_LEFT:
+            self.skip_round()
+
+    def handle_keydown(self, _event: pygame.event.EventType):
+        if _event.key == pygame.K_SPACE:
+            self.skip_round()
+
+    def skip_round(self):
+        if self.outcome:
+            self.att_swap.animation.reset()
+            self.show_outcome()
+            self.next_round()
+
+    def next_round(self):
+        self.at -= 1
+        self.round += 1
+        if self.dt > 0:
+            self.at, self.dt = self.dt, self.at
+            self.att_swap, self.def_swap = self.def_swap, self.att_swap
+        self.outcome = self.damage = None
 
     def loop(self, _events, dt):
         super().loop(_events, dt)
@@ -95,40 +126,30 @@ class BattleAnimation(room.Room):
                 animation.duration = 200
         elif animation.clock < -1000:
             animation.go_backward()
-            animation.reset()
             animation.callback = self.anim_finished
             animation.text.visible = False
-            self.at -= 1
-            self.round += 1
-            if self.dt > 0:
-                self.at, self.dt = self.dt, self.at
-                self.att_swap, self.def_swap = self.def_swap, self.att_swap
-            self.done = (self.at <= 0 and self.dt <= 0) or self.attacking.health <= 0 or self.defending.health <= 0
+            animation.invalidate()
+            self.next_round()
+        self.done = (self.at <= 0 and self.dt <= 0) or self.attacking.health <= 0 or self.defending.health <= 0
 
-    def exp_or_die(self, unit1, unit2):
+    @staticmethod
+    def exp_or_die(unit1: Unit, unit2: Unit) -> None:
         if unit1.health > 0:
             unit1.gain_exp(unit2)
-            room.run_room(ExperienceAnimation(unit1, center=display.get_rect().center))
+            room.run_room(ExperienceAnimation(unit1))
         else:
             s.loaded_map.kill_unit(unit1)
-
-    def broken_screen(self, unit):
-        sounds.play('broke')
-        broken_text = f.SMALL.render("%s is broken" % unit.weapon.name, True, c.RED)
-        display.window.blit(broken_text, center=broken_text.get_rect().center)
-        display.flip()
-        self.wait_event(timeout=3000)
 
     def end(self):
         super().end()
 
         if self.attacking.weapon and self.attacking.weapon.uses == 0:
-            self.broken_screen(self.attacking)
+            broken_screen(self.attacking)
         if self.defending.weapon and self.defending.weapon.uses == 0:
-            self.broken_screen(self.defending)
+            broken_screen(self.defending)
 
-        pygame.mixer.music.fadeout(500)
-        self.wait_event(500)
+        room.run_room(rooms.Fadeout(500))
+
         self.attacking.team.play_music('map', True)
 
         self.attacking.played = True
@@ -147,53 +168,62 @@ class BattleAnimation(room.Room):
 
 
 class ExperienceAnimation(gui.LinearLayout):
-    def __init__(self, unit, **kwargs):
-        super().__init__(bg_color=(0, 0, 0, 0), wait=False, allowed_events=[p.MOUSEBUTTONDOWN, p.KEYDOWN], **kwargs)
-        self.unit = unit
-        self.gained_exp = unit.gained_exp()
-        self.image = gui.Image(unit.image)
-        self.bar = gui.LifeBar(max=99, value=unit.prev_exp, blocks_per_row=100, block_size=(2, 10), life_color=c.YELLOW)
+    def __init__(self, _unit: Unit, **kwargs) -> None:
+        super().__init__(wait=False, gravity=Gravity.CENTER, allowed_events=[p.MOUSEBUTTONDOWN, p.KEYDOWN], **kwargs)
+        self.unit = _unit
+        self.gained_exp = _unit.exp_prev + _unit.gained_exp()
+        self.image = gui.Image(_unit.image, die_when_done=False, bg_color=c.MENU_BG)
+        self.bar = gui.LifeBar(max=99, value=_unit.exp_prev, blocks_per_row=100, block_size=(2, 10), life_color=c.YELLOW)
         self.label = gui.Label(_("EXP: {experience}") + "\t" + _("LV: {level}"), f.SMALL, txt_color=c.YELLOW)
-        self.label.format(**unit.__dict__)
+        self.label.format(**_unit.__dict__)
         self.add_children(self.image, self.bar, self.label)
+        self.time = 0
 
     def begin(self):
         super().begin()
         sounds.play('exp', -1)
+        self.bind_keys((p.K_RETURN, p.K_SPACE), self.handle_event)
+        self.bind_click((1,), self.handle_event)
 
     def loop(self, _events, dt):
         super().loop(_events, dt)
-        self.bar.value += 1
-        if self.unit.levelled_up() and self.bar.value % 100 == 0:
-            sounds.play('levelup')
-        self.label.format(**self.unit.__dict__)
-        self.done = self.bar.value >= self.gained_exp
+        self.time += dt
+        if self.bar.value < self.gained_exp:
+            self.bar.value += 1
+            if self.unit.levelled_up() and self.bar.value % 100 == 0:
+                sounds.play('levelup')
+            self.label.format(**self.unit.__dict__)
+        else:
+            if self.time > 2000:
+                self.done = True
+            elif self.time > 100:
+                sounds.stop('exp')
 
-    def handle_mousebuttondown(self, event):
+    def handle_event(self, _event):
         self.bar.value = self.gained_exp
 
-    def handle_keydown(self, event):
-        self.bar.value = self.gained_exp
 
-    def end(self):
-        super().end()
-        sounds.stop('exp')
-        self.wait_event(timeout=2000)
-
-
-if __name__ == '__main__':
-    import gettext, logging
-    import unit, resources
+def test_battle_animation():
+    import gettext
+    import logging
+    import resources
     gettext.install('ice-emblem', resources.LOCALE_PATH)
-    logging.basicConfig(level=20)
+    logging.basicConfig(level=80)
     s.load_map('resources/maps/default.tmx')
-    unit1 = unit.Unit(name='Soldier', health=30, level=1, experience=99, strength=5, skill=500, speed=1, luck=2, defence=1, resistence=1, movement=1, constitution=1, aid=1, affinity=None, condition=None, wrank=[])
-    unit2 = unit.Unit(name='Skeleton', health=31, level=1, experience=0, strength=6, skill=30, speed=10, luck=1, defence=1, resistence=1, movement=1, constitution=1, aid=1, affinity=None, condition=None, wrank=[])
+    unit1 = Unit(name='Soldier', health=30, level=1, experience=99, strength=5, skill=500, speed=1, luck=2, defence=1,
+                 resistance=1, movement=1, constitution=1, aid=1, affinity=None, wrank=[])
+    unit2 = Unit(name='Skeleton', health=31, level=1, experience=0, strength=6, skill=30, speed=10, luck=1, defence=1,
+                 resistance=1, movement=1, constitution=1, aid=1, affinity=None, wrank=[])
     unit1.coord = (1, 1)
     unit2.coord = (1, 2)
-    team1 = unit.Team('Ones', (255, 0, 0), 0, [unit1], unit1, {})
-    team2 = unit.Team('Twos', (0, 0, 255), 0, [unit2], unit2, {})
+    team1 = Team('Ones', (255, 0, 0), 0, [unit1], unit1, {})
+    team2 = Team('Twos', (0, 0, 255), 0, [unit2], unit2, {})
     s.units_manager.units = [unit1, unit2]
+    display.window.fill(c.GREY)
     while unit1.health > 0 and unit2.health > 0:
         room.run_room(BattleAnimation(unit1, unit2))
         unit1, unit2 = unit2, unit1
+
+
+if __name__ == '__main__':
+    test_battle_animation()
